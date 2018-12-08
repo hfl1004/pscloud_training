12298f004de2be451c110a88af6ae75c28da8916
hfl1004 <45680164+hfl1004@users.noreply.github.com>
2018-12-08 16:21:59 +0000

    hflday1

--- pscloud_training/models/.ipynb_checkpoints/lesson-checkpoint.py
+++ pscloud_training/models/.ipynb_checkpoints/lesson-checkpoint.py
@@ -5,7 +5,7 @@ class TrainingLesson(models.Model):
     _description = "课程信息"
 
     name = fields.Char(string='Name')
-    teacher_id = fields.Many2one('res.partner', string='老师')
-    student_ids = fields.Many2many('res.partner', string='学生', readonly=True)
+  #  teacher_id = fields.Many2one('res.partner', string='老师')
+  #  student_ids = fields.Many2many('res.partner', string='学生', readonly=True)
     start_date = fields.Date(string='开始时间')
     end_date = fields.Date(string='结束时间')
